73bb160eece10a0e873364fdc5e1249b5d256fd7
HoovyBeast05 <hussein05.ae@alumnes.ilerna.com>
2024-11-14 17:05:10 +0100

    Making the final design

---
+++ src/colgado/Colgado1.java
@@ -0,0 +1,21 @@
+package colgado;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Colgado1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Welcome to the hanged man!!");
+		System.out.println("------------------------------------");
+		System.out.println("------------------------------------");
+		
+		
+
+	}
+
+}
