8fb6a96ba6c662cd056049d03637b911563b87c6
HoovyBeast05 <hussein05.ae@alumnes.ilerna.com>
2024-11-14 15:43:48 +0100

    primero

---
+++ src/colgado/TestArray.java
@@ -0,0 +1,16 @@
+package colgado;
+
+import java.util.Random;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Random r = new Random();
+		
+		String[] words = {"cono", "pene", "puta", "madre"};
+
+	}
+
+}
